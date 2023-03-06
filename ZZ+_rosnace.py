88019e7bdb0be76a8b1a8191387b8e8aa7616902
Bartek <barkoz.2003@gmail.com>
2023-03-06 23:00:21 +0100

    f

--- ZZ+_rosnace.py
+++ ZZ+_rosnace.py
@@ -1,11 +1,11 @@
 def rosnace(n):
     return [i+1 for i in range(n)]
-n=50000
+n=30000
 #for i in range(10):
     #seq = rosnace(n)
     #print(seq)
 #print("Increasing sequence:")
-with open('ZZ10_rosnace_50000.txt', 'w') as f:
+with open('ZZ9_rosnace_30000.txt', 'w') as f:
     for i in range(1):
         r = rosnace(n)
         var_name = 'A{}'.format(i+1)
