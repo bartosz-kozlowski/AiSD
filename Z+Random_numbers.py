d40d6d042bbc973e8428d01e1352c0af94c0340c
Bartek <barkoz.2003@gmail.com>
2023-03-09 16:00:17 +0100

    update

--- Z+Random_numbers.py
+++ Z+Random_numbers.py
@@ -1,14 +1,14 @@
 import random
-
+A1=[]
 def randomowe_liczby(n):
     return [random.randint(1, 100000) for _ in range(n)]
-n=40000 #liczba elementow
-#for i in range(n):
-    #r = randomowe_liczby(n)
-    #print(r)
-with open('Z10_random_40000.txt', 'w') as f:
-    for i in range(1):
-        r = randomowe_liczby(n)
-        var_name = 'A{}'.format(i+1)
-        vars()[var_name] = r
-        f.write('{} = {}\n'.format(var_name, r))
+n=10 #liczba elementow
+for i in range(1):
+    A1 = randomowe_liczby(n)
+    print(A1)
+#with open('Z10_random_40000.txt', 'w') as f:
+    #for i in range(1):
+        #r = randomowe_liczby(n)
+           # var_name = 'A{}'.format(i+1)
+           # vars()[var_name] = r
+  #          f.write('{} = {}\n'.format(var_name, r))
---
+++ tt.py
@@ -0,0 +1,4 @@
+with open('ZZ7_rosnace_10000.txt', 'r') as A1:
+    for A1 in A1:
+        exec(A1)
+    print(A1)
